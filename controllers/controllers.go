101b1bbbb4528c6f8d7a750610e6e3744027a177
LeonardoCampello-dev <leonardocampello.dev@gmail.com>
2022-07-04 07:54:54 -0300

    creating student struct

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,14 +1,12 @@
 package controllers
 
 import (
+	"github.com/LeonardoCampello-dev/go-api-rest-with-gin/models"
 	"github.com/gin-gonic/gin"
 )
 
 func GetAllStudents(context *gin.Context) {
-	context.JSON(200, gin.H{
-		"id":   "1",
-		"name": "Leonardo",
-	})
+	context.JSON(200, models.Students)
 }
 
 func Salutation(context *gin.Context) {
