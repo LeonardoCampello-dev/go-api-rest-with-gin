39a2fd42ab7a7c9dcad8d19aed004fae9d5675e4
Leonardo Campello <leonardo.campello@moovin.com.br>
2022-07-18 09:33:59 -0300

    testing salutation route body

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/LeonardoCampello-dev/go-api-rest-with-gin/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func MakeRoutes() *gin.Engine {
@@ -26,7 +28,11 @@ func TestCheckSalutationStatusCode(test *testing.T) {
 
 	routes.ServeHTTP(response, req)
 
-	if response.Code != http.StatusOK {
-		test.Fatalf("received status: %d, expected status: %d", response.Code, 200)
-	}
+	assert.Equal(test, http.StatusOK, response.Code, "the request should return 200 if given a name parameter")
+
+	responseMock := `{"API says":"Hi fake-name, how are you?"}`
+
+	responseBody, _ := ioutil.ReadAll(response.Body)
+
+	assert.Equal(test, responseMock, string(responseBody))
 }
